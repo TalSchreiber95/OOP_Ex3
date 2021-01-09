e7de8c09a45e80bfacfd42e671ebe02bb9bf3b66
talschreiberpc <talfreestyle@gmail.com>
2021-01-09 14:02:13 +0200

    First DiGraph implementation.

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,41 +1,77 @@
+import self as self
+
 from GraphInterface import GraphInterface
+
+
+class GeoLocation(object):
+    def __init__(self, location=None):
+        self.location = location
+        if location is not None:
+            self.x = location[0]
+            self.y = location[1]
+            self.z = location[2]
+
+    def distance(self, other):
+        d1 = (self.x - other.x) ** 2  # (x1-x2)^2
+        d2 = (self.y - other.y) ** 2
+        d3 = (self.z - other.z) ** 2
+        return (d1 + d2 + d3) ** 0.5
+
+
+class NodeData(object):
+    def __init__(self, key: int, tag=0, info="", location=None, weight=1):
+        self.key = key
+        self.tag = tag
+        self.info = info
+        self.weight = weight
+        if location is not None:
+            self.location = GeoLocation(location)
+        else:
+            self.location = None
 
 
 class DiGraph(GraphInterface):
     """This abstract class represents an interface of a graph."""
+
+    def __init__(self):
+        self.nodes = dict()
+        self.outEdges = dict()
+        self.inEdges = dict()
+        self.sizeE = 0
+        self.sizeV = 0
+        self.sizeMc = 0
 
     def v_size(self) -> int:
         """
                   Returns the number of vertices in this graph
                   @return: The number of vertices in this graph
                   """
-
-        pass
+        return self.sizeV
 
     def e_size(self) -> int:
         """
                 Returns the number of edges in this graph
                 @return: The number of edges in this graph
                 """
-        pass
+        return self.sizeE
 
     def get_all_v(self) -> dict:
         """return a dictionary of all the nodes in the Graph, each node is represented using a pair
                     (node_id, node_data)
                    """
-        pass
+        return self.nodes
 
     def all_in_edges_of_node(self, id1: int) -> dict:
         """return a dictionary of all the nodes connected to (into) node_id ,
                 each node is represented using a pair (other_node_id, weight)
                  """
-        pass
+        return self.inEdges.get(id1)
 
     def all_out_edges_of_node(self, id1: int) -> dict:
         """return a dictionary of all the nodes connected from node_id , each node is represented using a pair
                (other_node_id, weight)
                """
-        pass
+        return self.outEdges.get(id1)
 
     def get_mc(self) -> int:
         """
@@ -43,7 +79,7 @@ class DiGraph(GraphInterface):
                    on every change in the graph state - the MC should be increased
                    @return: The current version of this graph.
                    """
-        pass
+        return self.sizeMc
 
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
         """
@@ -55,9 +91,20 @@ class DiGraph(GraphInterface):
 
                   Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
                   """
-        pass
+        if id1 not in self.nodes or id2 not in self.nodes or id1 == id2:
+            return False
+        if weight < 0:
+            return False
+        if id2 in self.outEdges.get(id1) or id1 in self.inEdges.get(id2):
+            return False  # If edge (src,dest) did not exist before, increment edgeSize.
+        self.sizeE += 1
+        self.sizeMc += 1
+        self.inEdges[id2][id1] = weight
+        self.outEdges[id1][id2] = weight
+        return True
 
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
+
         """
               Adds a node to the graph.
               @param node_id: The node ID
@@ -66,7 +113,14 @@ class DiGraph(GraphInterface):
 
               Note: if the node id already exists the node will not be added
               """
-        pass
+        if node_id in self.nodes:
+            return False
+        self.nodes[node_id] = NodeData(node_id, location=pos)
+        self.outEdges[node_id] = {}
+        self.inEdges[node_id] = {}
+        self.sizeV += 1
+        self.sizeMc += 1
+        return True
 
     def remove_node(self, node_id: int) -> bool:
         """
@@ -76,7 +130,20 @@ class DiGraph(GraphInterface):
 
                   Note: if the node id does not exists the function will do nothing
                   """
-        pass
+        if node_id not in self.nodes:
+            return False
+        for i in self.outEdges.keys():
+            if node_id in self.outEdges[i].keys():
+                del self.outEdges[i][node_id]
+        for j in self.inEdges.keys():
+            if node_id in self.inEdges[j].keys():
+                del self.outEdges[j][node_id]
+        self.outEdges.pop(node_id)
+        self.inEdges.pop(node_id)
+        self.nodes.pop(node_id)
+        self.sizeV -= 1
+        self.sizeMc += 1
+        return True
 
     def remove_edge(self, node_id1: int, node_id2: int) -> bool:
         """
@@ -87,4 +154,10 @@ class DiGraph(GraphInterface):
 
                  Note: If such an edge does not exists the function will do nothing
                  """
-        pass
+        if node_id1 in self.nodes and node_id2 in self.nodes and node_id2 != node_id1:
+            if node_id1 in self.outEdges or node_id1 in self.inEdges:
+                del self.outEdges[node_id1][node_id2]
+                del self.inEdges[node_id2][node_id1]
+                self.sizeE -= 1
+                return True
+        return False
